e4acd2ae6956d88a726af22bc27e868530cc7b09
riqgod <ham2@cin.ufpe.br>
2018-07-04 00:46:34 -0300

    Beta meta feito! (AJEITAR COLISÕES)

--- breakout.hs
+++ breakout.hs
@@ -15,17 +15,38 @@ module Main where
     import Graphics.Rendering.OpenGL (GLdouble)
     
     data GameAttribute = Score Int
+    data TileAttribute = NoTileAttribute
+
+    type BreakoutTile = Tile TileAttribute
+    type BreakoutMap = TileMatrix TileAttribute
+
      
     width = 800
     height = 800
     w = fromIntegral width :: GLdouble
     h = fromIntegral height :: GLdouble
     
+    magenta :: InvList
+    magenta = Just [(255,0,255)]
+
+    tileSize :: GLdouble
+    tileSize = 30.0
+
+    border1,free1 :: Int
+    border1 = 1
+    free1 = 2
+    block = 3
+
     main :: IO ()
     main = do
       let winConfig = ((100,80),(width,height),"Breakout 2.0")
-          bmpList = [("ivan.bmp", Nothing)]
-          gameMap = textureMap 0 30 30 w h
+          bmpList = [("ivan.bmp",      Nothing),
+                     ("border1.bmp",   magenta),
+                     ("free1.bmp",     magenta),
+                     ("block.bmp",     magenta)]
+
+          gameMap = multiMap [(tileMap map1 tileSize tileSize)] 0 
+
           bar     = objectGroup "barGroup"  [createBar]
           ball    = objectGroup "ballGroup" [createBall]
           block   = objectGroup "blockGroup" [createBlock]
@@ -46,7 +67,7 @@ module Main where
     createBlock =
       let blockBound = [(0,0),(0,30),(100,30),(100,0)]
           blockPic = Basic (Polyg blockBound 0 0 1.0 Filled)
-      in object "block" blockPic False (500,500) (0,0) ()
+      in object "block" blockPic True (500,500) (0,0) ()
 
 
     createBar :: GameObject ()
@@ -55,7 +76,7 @@ module Main where
           barPic   = Basic (Polyg barBound 1.0 1.0 1.0 Filled)
       in object "bar" barPic False (w/2,30) (0,0) ()
     
-    moveBarToRight :: Modifiers -> Position -> IOGame GameAttribute () () () ()
+    moveBarToRight :: Modifiers -> Position -> IOGame GameAttribute () () TileAttribute ()
     moveBarToRight _ _ = do
       obj     <- findObject "bar" "barGroup"
       (pX,pY) <- getObjectPosition obj
@@ -64,7 +85,7 @@ module Main where
        then (setObjectPosition ((pX + 20),pY) obj)
        else (setObjectPosition ((w - (sX/2)),pY) obj)
     
-    moveBarToLeft :: Modifiers -> Position -> IOGame GameAttribute () () () ()
+    moveBarToLeft :: Modifiers -> Position -> IOGame GameAttribute () () TileAttribute ()
     moveBarToLeft _ _ = do
       obj <- findObject "bar" "barGroup"
       (pX,pY) <- getObjectPosition obj
@@ -73,7 +94,7 @@ module Main where
         then (setObjectPosition ((pX - 20),pY) obj)
         else (setObjectPosition (sX/2,pY) obj)
     
-    gameCycle :: IOGame GameAttribute () () () ()
+    gameCycle :: IOGame GameAttribute () () TileAttribute ()
     gameCycle = do
       (Score n) <- getGameAttribute
       printOnScreen (show n) TimesRoman24 (0,0) 1.0 1.0 1.0
@@ -97,4 +118,37 @@ module Main where
                                      setGameAttribute (Score (n + 10)))
       showFPS TimesRoman24 (w-40,0) 1.0 0.0 0.0
     
-    
+    b,f,g :: BreakoutTile
+    b = (border1, True, 0.0, NoTileAttribute)
+    f = (free1,   False, 0.0, NoTileAttribute)
+    g = (block,  False, 0.0, NoTileAttribute)
+
+
+    map1 :: BreakoutMap
+    map1 = [[b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
+            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
+            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
+            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
+            [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b]]
