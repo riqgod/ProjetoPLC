055933c2b2c7fc5ca61702b0ecbe86b25770b78a
riqgod <ham2@cin.ufpe.br>
2018-07-04 16:53:47 -0300

    Base code and assets

--- breakout.hs
+++ breakout.hs
@@ -14,15 +14,22 @@ module Main where
     import Graphics.UI.Fungen
     import Graphics.Rendering.OpenGL (GLdouble)
     
-    data GameAttribute = Score Int
+    data GameAttribute = Score Int Int
     data TileAttribute = NoTileAttribute
 
+    type Blocks = GameObject()
+    type Ball = GameObject()
+    type BreakoutAction a = IOGame GameAttribute () () TileAttribute a
     type BreakoutTile = Tile TileAttribute
     type BreakoutMap = TileMatrix TileAttribute
 
+    rows = 4
+    columns = 5
+    boundsx = 30
+    boundsy = 20
      
     width = 800
-    height = 800
+    height = 900
     w = fromIntegral width :: GLdouble
     h = fromIntegral height :: GLdouble
     
@@ -32,36 +39,61 @@ module Main where
     tileSize :: GLdouble
     tileSize = 30.0
 
-    border1,free1 :: Int
+    border1,free1,ball,field :: Int
     border1 = 1
     free1 = 2
-    block = 3
+    ball = 3
+    field = 4
 
     main :: IO ()
     main = do
       let winConfig = ((100,80),(width,height),"Breakout 2.0")
-          bmpList = [("ivan.bmp",      Nothing),
+          bmpList = [("oi.bmp",      Nothing), 
                      ("border1.bmp",   magenta),
                      ("free1.bmp",     magenta),
-                     ("block.bmp",     magenta)]
+                     ("ball.bmp",     magenta),
+                     ("field.bmp",   Nothing)]
 
-          gameMap = multiMap [(tileMap map1 tileSize tileSize)] 0 
+          gameMap = multiMap [(tileMap map1 w h)] 0 
 
           bar     = objectGroup "barGroup"  [createBar]
-          ball    = objectGroup "ballGroup" [createBall]
-          block   = objectGroup "blockGroup" [createBlock]
-          initScore = Score 0
+          ball    = objectGroup "ballGroup" createBall
+          blocks  = objectGroup "blockGroup" initBlocks
+
+          initAttributes = (Score 0 (rows * ((columns*2)-1) ))
           input = [
             (SpecialKey KeyRight, StillDown, moveBarToRight)
             ,(SpecialKey KeyLeft,  StillDown, moveBarToLeft)
             ,(Char 'q',            Press,     \_ _ -> funExit)
             ]
-      funInit winConfig gameMap [bar,ball,block] () initScore input gameCycle (Timer 30) bmpList
+      funInit winConfig gameMap [bar,ball,blocks] () initAttributes input gameCycle (Timer 8) bmpList
     
-    createBall :: GameObject ()
+    createBlocks :: Int -> Int -> [Blocks]
+    createBlocks row column
+     | (row>= rows) = []
+     | (column >= columns) = createBlocks (row+1) (-4)
+     | otherwise = do
+      let offsetx = boundsx*2+5
+      let offsety = boundsy*2+5
+      let x = (w/2)+(offsetx * (fromIntegral column))
+      let y = (h - 300)+((fromIntegral row) * offsety)
+      let name = "block" ++ (show row) ++ (show column)
+      (createBlockAt (x, y) (name):(createBlocks (row) (column+1)) )
+
+    createBlockAt :: (GLdouble, GLdouble) -> String -> Blocks
+    createBlockAt (pX, pY) name =
+       let blockBounds = [(-boundsx,-boundsy),(boundsx,-boundsy),(boundsx,boundsy),(-boundsx,boundsy)]
+           blockPoly = Basic (Polyg blockBounds 0.0 0.0 1.0 Filled)
+       in object name blockPoly False (pX,pY) (0,0) ()
+  
+    initBlocks :: [Blocks]
+    initBlocks = (createBlocks 0 (-4) )
+
+    createBall :: [GameObject ()]
     createBall =
-      let ballPic = Basic (Circle 10.0 1.0 1.0 1.0 Filled)
-      in object "ball" ballPic False (w/2,h/2) (-8,8) ()
+      let ballPic = Tex (tileSize,tileSize) ball
+      in [(object "ball1" ballPic False (400,100) (2,-2) ()),
+          (object "ball2" ballPic False (400,100) (2,-2) ())]
     
     createBlock :: GameObject()
     createBlock =
@@ -94,61 +126,101 @@ module Main where
         then (setObjectPosition ((pX - 20),pY) obj)
         else (setObjectPosition (sX/2,pY) obj)
     
+
+    checkBlockCollision :: Ball -> [Blocks] -> BreakoutAction()
+    checkBlockCollision ball [] = do return()
+    checkBlockCollision ball (x:xs) = do
+      (bX,bY) <- getObjectPosition ball
+      (pX,pY) <- getObjectPosition x
+      (ballSizeX,ballSizeY) <- getObjectSize ball
+      (sX,sY) <- getObjectSize x
+      col <- objectsCollision ball x
+      let (pSizeX,pSizeY) = (sX/2,sY/2)
+      let (bSizeX,bSizeY) = (ballSizeX/2,ballSizeY/2)
+      --liftIOtoIOGame(putStrLn(show (length xs)))               
+      when(col) (do 
+        setObjectAsleep True x
+        checkBallPos ball (bX,bY) (pX,pY) (bSizeX,bSizeY) (pSizeX,pSizeY))
+      checkBlockCollision ball xs
+            
+    -- ball -> block
+    checkBallPos :: Ball -> (GLdouble, GLdouble) -> (GLdouble, GLdouble) -> (GLdouble,GLdouble) -> (GLdouble,GLdouble) -> BreakoutAction ()
+    checkBallPos ball (bX, bY) (pX, pY) (bSizeX,bSizeY) (pSizeX,pSizeY) = do
+      (Score n blocks) <- getGameAttribute
+      (vx,vy) <- getObjectSpeed ball
+      if((pY-bY)>=(pSizeY+bSizeY-vy))
+        then( do
+          reverseYSpeed ball
+          liftIOtoIOGame(putStrLn( "primeiro if")))
+        else if((bY-pY)>=(pSizeY+bSizeY+vy))
+          then(do
+            reverseYSpeed ball
+            liftIOtoIOGame(putStrLn( "second if")))
+          else(do
+            reverseXSpeed ball
+            liftIOtoIOGame(putStrLn( "else")))
+      {-if( (Vx>0) && (Vy>0) )
+        then()
+        else if((Vx<0) && (Vy>0) )
+          then()
+          else if( (Vx>0) && (Vy<0) )
+            then()
+            else if( (Vx<0) && (Vy<0) )
+              then()-}
+
+
+
+      {-if((bX) == (pX+pSizeX+bSizeX)) || ((bX) == (pX-pSizeX-bSizeX) )
+        then(do 
+             reverseXSpeed ball
+             setGameAttribute(Score (n+10) blocks ))
+        else if( ((bY)==(pY+pSizeY+bSizeY)) || ((bY)==(pY-pSizeY-bSizeY)) )
+          then(do 
+               reverseYSpeed ball
+               setGameAttribute(Score (n+10) blocks ))
+          else (do
+                setGameAttribute(Score n blocks ))-}
+        
+    checkBalls :: [Ball] -> BreakoutAction()
+    checkBalls [] = return()
+    checkBalls (x:xs) = do
+      (Score n blocks) <- getGameAttribute
+      flags <- getObjectsFromGroup "blockGroup"
+      checkBlockCollision x flags
+      col1 <- objectLeftMapCollision x
+      col2 <- objectRightMapCollision x
+      when (col1 || col2) (reverseXSpeed x)     
+
+      col3 <- objectTopMapCollision x
+      when col3 (reverseYSpeed x)
+      col4 <- objectBottomMapCollision x
+      when (col4) (do
+        if( (n-100) < 0 )
+          then (do
+            setGameAttribute (Score (n-100) blocks)
+            reverseYSpeed x)
+          else(do 
+               setGameAttribute (Score (n-100) blocks) 
+               reverseYSpeed x))
+      bar <- findObject "bar" "barGroup"
+      col5 <- objectsCollision x bar
+      let (_,vy) = getGameObjectSpeed x
+      when (and [col5, vy < 0])  (do reverseYSpeed x
+                                     setGameAttribute (Score (n + 10) blocks))
+      checkBalls xs
+
+
     gameCycle :: IOGame GameAttribute () () TileAttribute ()
     gameCycle = do
-      (Score n) <- getGameAttribute
-      printOnScreen (show n) TimesRoman24 (0,0) 1.0 1.0 1.0
-      block <- findObject "block" "blockGroup"
-      ball <- findObject "ball" "ballGroup"
-      col1 <- objectLeftMapCollision ball
-      col2 <- objectRightMapCollision ball
-      when (col1 || col2) (reverseXSpeed ball)
-      col3 <- objectTopMapCollision ball
-      when col3 (reverseYSpeed ball)
-      col4 <- objectBottomMapCollision ball
-      when col4 $ do
-        -- funExit
-        setGameAttribute (Score 0)
-        reverseYSpeed ball
+      (Score n blocks) <- getGameAttribute
+      balls <- getObjectsFromGroup "ballGroup"
+      checkBalls balls
+      printOnScreen (show n) TimesRoman24 (750,750) 0.0 0.0 0.0
+      showFPS TimesRoman24 (w-40,0) 0.0 0.0 1.0
     
-      bar <- findObject "bar" "barGroup"
-      col5 <- objectsCollision ball bar
-      let (_,vy) = getGameObjectSpeed ball
-      when (and [col5, vy < 0])  (do reverseYSpeed ball
-                                     setGameAttribute (Score (n + 10)))
-      showFPS TimesRoman24 (w-40,0) 1.0 0.0 0.0
-    
-    b,f,g :: BreakoutTile
-    b = (border1, True, 0.0, NoTileAttribute)
-    f = (free1,   False, 0.0, NoTileAttribute)
-    g = (block,  False, 0.0, NoTileAttribute)
+    f :: BreakoutTile
+    f = (field, False, 0.0, NoTileAttribute)
 
 
     map1 :: BreakoutMap
-    map1 = [[b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
-            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
-            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
-            [b,f,f,f,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,b],
-            [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b]]
+    map1 = [[f]]
